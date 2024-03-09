87110910b65e7eb884eb0a6b2e9192ec6b0e4e37
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-03-09 14:57:41 +0900

    delete機能を実装しました。

---
+++ src/resources/views/subscriptionlist.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @if (session('message'))
+        <p>{{ session('message') }}</p>
+    @endif
+    <h1>Subscription List</h1>
+    <table>
+        <tr>
+            <th>Subscription Name</th>
+            <th>Icon</th>
+            <th>Color</th>
+            <th>Cancel URL</th>
+            <th>delete</th>
+        </tr>
+        @foreach ($subscriptions as $subscription)
+            <tr>
+                <td>{{ $subscription->subscription_name }}</td>
+                <td>{{ $subscription->icon }}</td>
+                <td>{{ $subscription->color }}</td>
+                <td>{{ $subscription->cancel_url }}</td>
+                <td>
+                    <form action="/subscriptions/{{ $subscription->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+</body>
+</html>
